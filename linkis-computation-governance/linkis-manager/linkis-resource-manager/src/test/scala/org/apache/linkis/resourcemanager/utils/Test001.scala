76dc90127053b932734ea6043cbf755cce48f1d0
husofskyzy <15997168992@163.com>
2022-03-07 14:36:40 +0800

    change return type from xml to json

---
+++ linkis-computation-governance/linkis-manager/linkis-resource-manager/src/test/scala/org/apache/linkis/resourcemanager/utils/Test001.scala
@@ -0,0 +1,10 @@
+package org.apache.linkis.resourcemanager.utils
+
+import org.junit.jupiter.api.Test
+class Test001 {
+
+  @Test
+  def connection_Equals_itself() {
+    println("lucy is here")
+  }
+}
