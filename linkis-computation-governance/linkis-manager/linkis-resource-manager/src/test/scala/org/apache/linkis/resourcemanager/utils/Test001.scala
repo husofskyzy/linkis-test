4da38bb8e15e8f18f06287356844150415e20a91
husofskyzy <15997168992@163.com>
2022-03-07 14:49:49 +0800

    submit

---
+++ linkis-computation-governance/linkis-manager/linkis-resource-manager/src/test/scala/org/apache/linkis/resourcemanager/utils/Test001.scala
@@ -0,0 +1,11 @@
+package org.apache.linkis.resourcemanager.utils
+
+import org.junit.jupiter.api.Test
+class Test001 {
+
+  @Test
+  def connection_Equals_itself() {
+    println("lucy is here")
+  }
+}
+
